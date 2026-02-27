2e7a7ae17c22bd73429a3e5779ba8ed53a6b1b18
gasanthosh10 <gasanthosh10@gmail.com>
2026-02-27 10:13:09 +0530

    UC2 Hardcoded Palindrome implemented

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,7 +1,25 @@
 public class PalindroneCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App Management System")
-    }
-    }
 
+        // Step 1: Hardcoded string
+        String word = "madam";
 
+        // Step 2: Variable to store reversed string
+        String reverse = "";
+
+        // Step 3: Reverse the string using loop
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        // Step 4: Compare original and reversed
+        if(word.equals(reverse)) {
+            System.out.println("The given word \"" + word + "\" is a PALINDROME.");
+        }
+        else {
+            System.out.println("The given word \"" + word + "\" is NOT a palindrome.");
+        }
+
+    }
+}
