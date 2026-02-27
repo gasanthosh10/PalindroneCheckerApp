9f96b53956139cea213b004d5b0f02ef87efc76b
gasanthosh10 <gasanthosh10@gmail.com>
2026-02-27 11:12:26 +0530

    UC9 Recursive palindrome checker implemented

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,36 +1,29 @@
 public class PalindroneCheckerApp {
 
+    // Recursive function
+    static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if(start >= end)
+            return true;
+
+        // If mismatch
+        if(str.charAt(start) != str.charAt(end))
+            return false;
+
+        // Recursive call (move inward)
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
 
-        // Step 1: Original input
-        String input = "A man a plan a canal Panama";
+        String word = "madam";
 
-        // Step 2: Normalize string
-        // remove spaces & special characters, convert to lowercase
-        String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        boolean result = isPalindrome(word, 0, word.length() - 1);
 
-        // Step 3: Check palindrome using two-pointer
-        int start = 0;
-        int end = normalized.length() - 1;
-
-        boolean isPalindrome = true;
-
-        while(start < end) {
-
-            if(normalized.charAt(start) != normalized.charAt(end)) {
-                isPalindrome = false;
-                break;
-            }
-
-            start++;
-            end--;
-        }
-
-        // Step 4: Display result
-        if(isPalindrome) {
-            System.out.println("\"" + input + "\" is a PALINDROME (ignoring case & spaces).");
-        } else {
-            System.out.println("\"" + input + "\" is NOT a palindrome.");
-        }
+        if(result)
+            System.out.println("The string \"" + word + "\" is a PALINDROME.");
+        else
+            System.out.println("The string \"" + word + "\" is NOT a palindrome.");
     }
 }
